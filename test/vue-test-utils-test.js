806983ab182c2f5f348e4b8c839e6bd0a7b05068
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2020-11-13 20:30:40 +0100

    Update vue-test-utils-test.js

--- test/vue-test-utils-test.js
+++ test/vue-test-utils-test.js
@@ -1,39 +1,22 @@
 // # vue-test-utils-test.js
 import path from 'path';
-import Vue from 'vue';
 import { mount } from '@vue/test-utils';
 import { expect } from './chai.js';
+import Person from './files/person.vue';
 
 describe('Using vue-test-utils', function() {
 
-	context('loading .vue files', function() {
+	it('a Person component', function() {
 
-		before(function() {
-			this.require = async function(id) {
-				let filePath = path.join('./files', id);
-				return await import(filePath);
-			};
-		});
-
-		beforeEach(async function() {
-			let { title } = this.currentTest;
-			let module = await this.require(title);
-			this.component = module.default;
-		});
-
-		it('person.vue', function() {
-
-			let view = mount(this.component, {
-				propsData: {
-					person: {
-						firstName: 'John',
-						lastName: 'Doe',
-					},
+		let view = mount(Person, {
+			propsData: {
+				person: {
+					firstName: 'John',
+					lastName: 'Doe',
 				},
-			});
-			expect(view.vm.fullName).to.equal('John Doe');
-
+			},
 		});
+		expect(view.vm.fullName).to.equal('John Doe');
 
 	});
 
