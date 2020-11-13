a479b006393b88821142ffac7436f0aadf983817
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2020-11-13 17:56:39 +0100

    Test functional components

--- test/loader-test.js
+++ test/loader-test.js
@@ -7,65 +7,53 @@ describe('The vue esm loader', function() {
 	context('loads .vue files', function() {
 
 		before(function() {
-
 			this.require = async function(id) {
 				let filePath = path.join('./files', id);
 				return await import(filePath);
 			};
-
 		});
 
 		beforeEach(async function() {
-
 			let { title } = this.currentTest;
 			let module = await this.require(title);
 			this.component = module.default;
-
 		});
 
 		it('script.vue', function() {
-
 			expect(this.component.data()).to.eql({
 				foo: 'bar',
 			});
-
 		});
 
 		it('template.vue', function() {
-
 			expect(this.component.render).to.be.a('function');
 			expect(this.component.staticRenderFns).to.be.an('array');
 			expect(this.component._compiled).to.be.true;
-
 		});
 
 		it('nested.vue', async function() {
-
 			let { components } = this.component;
 			let { default: tpl } = await this.require('template.vue');
 			expect(components.Component).to.equal(tpl);
-
 		});
 
 		it('scoped.vue', function() {
-
 			expect(this.component._scopedId).to.be.ok;
-			
+		});
+
+		it.only('functional.vue', function() {
+			expect(this.component.functional).to.be.true;
 		});
 
 		it('external-script.vue', function() {
-
 			expect(this.component.data()).to.eql({
 				foo: 'bar',
 			});
-
 		});
 
 		it('external-template.vue', function() {
-
 			let { render } = this.component;
 			expect(render).to.be.a('function');
-
 		});
 
 	});
