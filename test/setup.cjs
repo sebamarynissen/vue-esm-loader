c25c02ff0475149cdde17f345bf2e12d6d942ac7
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2023-03-22 15:30:43 +0100

    Test with jsdom

--- test/setup.cjs
+++ test/setup.cjs
@@ -3,6 +3,8 @@
 const pkg = require(require.resolve('vue/package.json'));
 if (pkg.version.startsWith('2')) {
 	two();
+} else {
+	three();
 }
 
 function two() {
@@ -14,4 +16,12 @@ function two() {
 	const Vue = require('vue');
 	Vue.config.productionTip = false;
 	Vue.config.devtools = false;
+}
+
+function three() {
+	require('jsdom-global')(undefined, {
+		pretendToBeVisual: true,
+		url: 'http://localhost',
+	});
+	globalThis.SVGElement = class {};
 }
--- test/vue-test-utils-test.js
+++ test/vue-test-utils-test.js
@@ -2,13 +2,24 @@
 import { mount } from '@vue/test-utils';
 import { expect } from 'chai';
 import Person from './files/person.vue';
+import { major } from '../lib/vue-version.js';
 
 describe('Using vue-test-utils', function() {
 
+	before(function() {
+		this.mount = function(Component, opts = {}) {
+			let { props, propsData, ...rest } = opts;
+			return mount(Component, {
+				[major === 2 ? 'propsData' : 'props']: props,
+				...rest,
+			});
+		};
+	});
+
 	it('a Person component', function() {
 
-		let view = mount(Person, {
-			propsData: {
+		let view = this.mount(Person, {
+			props: {
 				person: {
 					firstName: 'John',
 					lastName: 'Doe',
@@ -18,5 +29,4 @@ describe('Using vue-test-utils', function() {
 		expect(view.vm.fullName).to.equal('John Doe');
 
 	});
-
 });
