8caf3ffd44401a787e99b52dd56d78751b3f9478
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2023-03-22 13:34:01 +0100

    Restore support for Vue 2.6

--- test/loader-test.js
+++ test/loader-test.js
@@ -1,50 +1,69 @@
 // # loader-test.js
 import path from 'node:path';
+import { createRequire } from 'node:module';
+import semver from 'semver';
 import Vue from 'vue';
 import { expect } from 'chai';
+const require = createRequire(import.meta.url);
 
 describe('The vue esm loader', function() {
+
+	before(function() {
+		let version;
+		Object.defineProperty(this, 'version', {
+			get() {
+				if (version) return version;
+				let pkg = require(require.resolve('vue/package.json'));
+				return (version = pkg.version);
+			},
+		});
+
+		this.semver = function(expr) {
+			let { version } = this;
+			if (!semver.satisfies(this.version, expr)) {
+				this.skip();
+			}
+		};
+
+	});
 
 	context('loads .vue files', function() {
 
 		before(function() {
-			this.require = async function(id) {
+			this.require = async function(id = this.test.title) {
 				let filePath = path.join('./files', id);
-				return await import(filePath);
+				return (await import(filePath)).default;
 			};
 		});
 
-		beforeEach(async function() {
-			let { title } = this.currentTest;
-			let module = await this.require(title);
-			this.component = module.default;
-		});
-
-		it('script.vue', function() {
-			expect(this.component.data()).to.eql({
+		it('script.vue', async function() {
+			const component = await this.require();
+			expect(component.data()).to.eql({
 				foo: 'bar',
 			});
 		});
 
-		it('template.vue', function() {
-			expect(this.component.render).to.be.a('function');
-			expect(this.component.staticRenderFns).to.be.an('array');
-			expect(this.component._compiled).to.be.true;
+		it('template.vue', async function() {
+			const component = await this.require();
+			expect(component.render).to.be.a('function');
+			expect(component.staticRenderFns).to.be.an('array');
+			expect(component._compiled).to.be.true;
 		});
 
 		it('nested.vue', async function() {
-			let { components } = this.component;
-			let { default: tpl } = await this.require('template.vue');
+			let { components } = await this.require();
+			let tpl = await this.require('template.vue');
 			expect(components.Component).to.equal(tpl);
 		});
 
-		it('scoped.vue', function() {
-			expect(this.component._scopedId).to.be.ok;
+		it('scoped.vue', async function() {
+			const component = await this.require();
+			expect(component._scopedId).to.be.ok;
 		});
 
-		it('functional.vue', function() {
-			expect(this.component.functional).to.be.true;
-			const { component } = this;
+		it('functional.vue', async function() {
+			const component = await this.require();
+			expect(component.functional).to.be.true;
 			let props = { foo: 'bar' };
 			let main = new Vue({
 				render(h) {
@@ -54,20 +73,23 @@ describe('The vue esm loader', function() {
 			let el = main.$mount();
 		});
 
-		it('setup.vue', function() {
-			const { component } = this;
+		it('setup.vue', async function() {
+			this.semver('>=2.7');
+			const component = await this.require();
 			expect(component.foo).to.equal('bar');
 			expect(component.setup().foo).to.equal('baz');
 		});
 
-		it('external-script.vue', function() {
-			expect(this.component.data()).to.eql({
+		it('external-script.vue', async function() {
+			const component = await this.require();
+			expect(component.data()).to.eql({
 				foo: 'bar',
 			});
 		});
 
-		it('external-template.vue', function() {
-			let { render } = this.component;
+		it('external-template.vue', async function() {
+			const component = await this.require();
+			let { render } = component;
 			expect(render).to.be.a('function');
 		});
 
