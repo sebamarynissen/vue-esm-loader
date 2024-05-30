348dce9a10dc0c69e11c0809bec364168c38008a
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2024-05-30 22:40:27 +0200

    Fix tests on all versions

--- test/setup-jsdom.js
+++ test/setup-jsdom.js
@@ -14,3 +14,11 @@ const keys = [
 Object.assign(globalThis, Object.fromEntries(
 	keys.map(key => [key, dom.window[key]]),
 ));
+
+// Apparently Node 22 has a `navigator` global, so we can't just blindly copy it 
+// anymore.
+if (!('navigator' in globalThis)) {
+	globalThis.navigator = dom.window.navigator;
+} else {
+	Object.assign(globalThis.navigator, dom.window.navigator);
+}
