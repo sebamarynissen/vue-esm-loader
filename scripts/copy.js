d7a11363d0454f9e08be4feb6436e78836667c61
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2024-05-30 22:24:07 +0200

    Update copy.js

--- scripts/copy.js
+++ scripts/copy.js
@@ -1,5 +1,7 @@
 import fs from 'node:fs';
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+const dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const files = [
 	'README.md',
@@ -8,8 +10,8 @@ const files = [
 
 for (let file of files) {
 	fs.copyFileSync(
-		path.resolve(import.meta.dirname, '..', file),
-		path.resolve(import.meta.dirname, '../packages/vue-esm-loader', file),
+		path.resolve(dirname, '..', file),
+		path.resolve(dirname, '../packages/vue-esm-loader', file),
 	);
 	console.log(`Copied ${file}`);
 }
